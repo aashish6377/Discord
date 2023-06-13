629009b8bb5ebff8ff04ef14f73f3d4dda086ad9
aashish6377 <68929785+aashish6377@users.noreply.github.com>
2023-06-13 23:23:31 +0530

    Resolved an error with embed color in help.js

--- commands/help.js
+++ commands/help.js
@@ -1,16 +1,18 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 
 module.exports = {
-  name: 'help',
-  aliases: ['h', 'cmd', 'command'],
+  name: "help",
+  aliases: ["h", "cmd", "command"],
   run: async (client, message) => {
     message.channel.send({
       embeds: [
         new Discord.EmbedBuilder()
-          .setTitle('Commands')
-          .setDescription(client.commands.map(cmd => `\`${cmd.name}\``).join(', '))
-          .setColor('BLURPLE')
-      ]
-    })
-  }
-}
+          .setTitle("Commands")
+          .setDescription(
+            client.commands.map((cmd) => `\`${cmd.name}\``).join(", ")
+          )
+          .setColor("#1ABC9C"),
+      ],
+    });
+  },
+};
